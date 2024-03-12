332d8b705654341147944dcd18a650d6b64b4503
Stephen Weber <weber-stephen@users.noreply.github.com>
2024-03-11 22:00:59 -0700

    Installed Storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,35 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  webpackFinal: async (config) => {
+    if (config.resolve) {
+         config.resolve.alias = {
+              ...config.resolve.alias,
+              "@": path.resolve(__dirname, "../src/"),
+              components: path.resolve(__dirname, "../src/components"),
+              styles: path.resolve(__dirname, "../src/styles"),
+              constants: path.resolve(__dirname, "../src/constants"),
+         };
+    }
+    return config;
+  },
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "@storybook/addon-styling-webpack"
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  staticDirs: ["../public"],
+};
+export default config;
