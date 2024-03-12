1e3ff68cc80ed75eb89b4ede80119e95c8ea2c87
Stephen Weber <weber-stephen@users.noreply.github.com>
2024-03-11 21:36:04 -0700

    Added GraphQL

---
+++ src/app/api/graphql/route.ts
@@ -0,0 +1,28 @@
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { ApolloServer } from "@apollo/server";
+import { NextRequest } from "next/server";
+import { gql } from "graphql-tag";
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!",
+  },
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+// Typescript: req has the type NextRequest
+const handler = startServerAndCreateNextHandler<NextRequest>(server, {
+    context: async req => ({ req }),
+});
+
+export { handler as GET, handler as POST };
