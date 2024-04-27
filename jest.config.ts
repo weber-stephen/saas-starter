0150c820c620ef6ba815611d10a71a2b8de07a13
Stephen Weber <weber-stephen@users.noreply.github.com>
2024-04-27 16:16:27 -0700

    Fixing test paths and conflicts

--- jest.config.ts
+++ jest.config.ts
@@ -161,10 +161,9 @@ const config: Config = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  // testMatch: [
-  //   "**/__tests__/**/*.[jt]s?(x)",
-  //   "**/?(*.)+(spec|test).[tj]s?(x)"
-  // ],
+  testMatch: [
+    "**/src/**/?(*.)+(spec|test).[tj]s?(x)"
+  ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
